9a45c42c03fdb595167acdc2bf4e81c1c8a316f9
PrathamGupta423 <Prathamgupta423@gmail.com>
2024-03-18 17:13:09 +0530

    created Commit builder

---
+++ utils/commit_builder.py
@@ -0,0 +1,29 @@
+import datetime
+import pathlib as Path
+import Hash
+
+class Commit:
+    def __init__(self,blob_hash, parent_hash, author_name, committer_name, commit_message):
+        
+        self.blob_hash = blob_hash
+        self.parent_hash = parent_hash
+        self.author_name = author_name
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.timezone = datetime.datetime.now().astimezone().tzname()
+        self.committer_name = committer_name
+        self.commit_message = commit_message
+        self.name = Hash.hash_string(str(self))
+
+    def __str__(self):
+        return f"blob {self.blob_hash}\nparent {self.parent_hash}\nauthor {self.author_name} {self.timestamp} {self.timezone}\ncommitter {self.committer_name}\n{self.commit_message}"
+    
+    def save(self):
+        temp = Path.Path(".sfit/objects") / self.name  # ".sfit/objects" to be replaced with the absolute path to the objects directory
+        with temp.open("w") as f:
+            f.write(str(self))
+        print(f"Commit saved to {temp}")
+
+
+
+if __name__ == "__main__":
+    print("Invalid use")
