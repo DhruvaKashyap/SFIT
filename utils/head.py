f74cdf2235d9a70451719705cfcea042203f5eb3
Siddharth Reddy <141948050+SidZRed@users.noreply.github.com>
2024-03-22 16:33:37 +0530

    Add type hints to head.py

--- utils/head.py
+++ utils/head.py
@@ -1,10 +1,10 @@
 class HEAD:
-    def __init__(self, path):
+    def __init__(self, path: str) -> None:
         self.path = path
 
-    def get_ref(self):
+    def get_ref(self) -> str:
         with open(self.path, 'r') as file:
-            ref = file.readline().strip()
+            ref: str = file.readline().strip()
             if ref.startswith('refs/heads/'):
                 # Return the file name in reference to refs/heads
                 return ref[len('refs/heads/'):]
@@ -12,15 +12,15 @@ class HEAD:
                 # Return the reference as is (when HEAD is 'detached')
                 return ref
 
-    def set_ref(self, ref):
+    def set_ref(self, ref: str) -> None:
         with open(self.path, 'w') as file:
             if ref.startswith('refs/'):
-                # For branch references, write the full reference
+                # For branch references, provide complete reference
                 file.write(ref)
             else:
                 # Relative reference
                 file.write('refs/heads/' + ref)
 
-    def show_ref(self):
+    def show_ref(self) -> str:
         # Useful for other CLI commands like log, status, so can be directly imported
         return self.get_ref()
