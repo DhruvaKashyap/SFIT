364bfed41a8d01a1f7959990a1a51f42cc6521d6
Adithya <adithyaka@iisc.ac.in>
2024-03-26 00:27:19 +0530

    created an outline of the commit function

--- commands/commit.py
+++ commands/commit.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+import hashlib
 
 def initialize(subparsers):
   subparser: ArgumentParser = subparsers.add_parser("commit", help="commit changes with the given commit message")
@@ -6,7 +7,31 @@ def initialize(subparsers):
   subparser.set_defaults(func=command_handler)
 
 def command_handler(args: Namespace):
-  ...
+
+  pathOfFolder = ""
+  if not os.path.exists(pathOfFolder+"/.sfit/"):
+    raise Exception("Repository has not been initialized. Use sfit init to initialize the repository")
+  #to be verified if head exists
+  if not os.path.exists(pathOfFolder+"/.sfit/HEAD"):
+    ...
+
+  fileName = ""
+  if os.path.exists(pathOfFolder+"/.sfit/config"):
+    f = open(pathOfFolder+"/.sfit/config", 'r')
+    fileName = f.read() # making the assumption that fileName also includes the file extension
+  f = open(pathOfFolder+fileName)
+  contentsInFile = f.read()
+  
+  #generating hash using sha-1 algorithm
+  commitHash = (hashlib.sha1(contentsInFile.encode())).hexdigest()
+
+  #creating commit file in .sfit/objects with hash as file name and contents as the original file contents
+  f = open(pathOfFolder+"/.sfit/objects/"+ hash, 'w')
+  f.write(contentsInFile)
+
+
+
+
 
 if __name__ == "__main__":
   print("invalid usage")
